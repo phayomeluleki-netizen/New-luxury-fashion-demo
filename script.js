66f303045104647eb852f4dc1e828618a5825445
phayomeluleki-netizen <phayomeluleki@gmail.com>
2026-01-13 01:09:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 // HERO SLIDER
-let slides = document.querySelectorAll('.slide');
-let currentSlide = 0;
+const hero = document.querySelector(".hero");
+
+const heroImages = [
+  "images/hero/hero1.jpg",
+  "images/hero/hero2.jpg",
+  "images/hero/hero3.jpg"
+];
+
+let currentIndex = 0;
+
+function changeHeroImage() {
+  hero.style.backgroundImage = `url(${heroImages[currentIndex]})`;
+  currentIndex = (currentIndex + 1) % heroImages.length;
+}
+
+// Load first image
+changeHeroImage();
+
+// Change image every 5 seconds
+setInterval(changeHeroImage, 5000);
 
 const nextSlide = () => {
   slides[currentSlide].classList.remove('active');
