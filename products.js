ed8da92ff5462ab17064e5cce49246951679c8d7
phayomeluleki-netizen <phayomeluleki@gmail.com>
2026-05-17 16:01:53 +0200

    Refactor image paths in products.js
    Updated image paths to remove the 'images/products/' prefix.

--- products.js
+++ products.js
@@ -6,34 +6,34 @@ const products = [
     name: "man formal shoes",
     price: "E799",
     category: "men",
-    image: "images/products/men/20260421_204930.png"
+    image: "men/20260421_204930.png"
   },
   {
     id: 2,
     name: "Premium Jacket",
     price: "E1,499",
     category: "men",
-    image: "images/products/men/mans t-shirts.jpg"
+    image: "mans t-shirts.jpg"
   },
   {
     id: 3,
     name: "Evening Dress",
     price: "E1,899",
     category: "women",
-    image: "images/products/women/IMG-20260107-WA1027.jpg"
+    image: "IMG-20260107-WA1027.jpg"
   },
   {
     id: 4,
     name: "Lydies Dress",
     price: "E1,299",
     category: "women",
-    image: "images/products/women/IMG-20260109-WA0882.jpg"
+    image: "IMG-20260109-WA0882.jpg"
   },
   {
     id: 5,
     name: "Designer Bag",
     price: "E2,499",
     category: "accessories",
-    image: "images/products/accessories/IMG-20260119-WA0568.jpg"
+    image: "IMG-20260119-WA0568.jpg"
   }
 ];
